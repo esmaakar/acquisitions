5a8855da67fdab0d3c934d0c933bfff9decf8421
esmaakar <esmaakar171@gmail.com>
2025-10-19 21:17:32 +0300

    implement CI/CD pipelines and Github Actions

--- src/app.js
+++ src/app.js
@@ -35,5 +35,8 @@ app.get('/api',(req,res)=>{
 });
 app.use('/api/auth', authRoutes);
 app.use('/api/users',usersRoutes);
+app.use((req,res)=>{
+    res.status(404).json({ error: 'route not found'});
+})
 
 export default app;
